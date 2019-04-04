7846a2e44d3509aab89ab94beaa96958217b8e74
TienTM <tienit04021997@gmail.com>
2019-04-05 00:05:20 +0700

    Update in 12:05 - 5/4/2019

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,4 @@
-<?php require_once __DIR__. "/layouts/header.php"; ?>
+<?php require_once __DIR__."/layouts/header.php"; ?>
 
             <!-- Page Heading Nội dung -->
             <div class="row">
@@ -267,4 +267,4 @@
                 </div>
             </div>
             <!-- /.row -->
-<?php require_once  __DIR__. "/layouts/footer.php"; ?>
+<?php require_once  __DIR__."/layouts/footer.php"; ?>
