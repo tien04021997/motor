4afe85698d853f6b4241ead6fcdedaf972ae80cc
TienTM <tienit04021997@gmail.com>
2019-04-04 23:50:38 +0700

    update

---
+++ layouts/footer.php
@@ -0,0 +1,24 @@
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<!-- jQuery -->
+<script src="/motor/public/admin/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="/motor/public/admin/js/bootstrap.min.js"></script>
+
+<!-- Morris Charts JavaScript -->
+<script src="/motor/public/admin/js/plugins/morris/raphael.min.js"></script>
+<script src="/motor/public/admin/js/plugins/morris/morris.min.js"></script>
+<script src="/motor/public/admin/js/plugins/morris/morris-data.js"></script>
+
+</body>
+
+</html>
